cfc1b8ec973dd54546bd27cd5b47b48ce0067bc2
huangtong123456 <1985753854@qq.com>
2019-12-29 19:21:47 +0800

    work18

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
-const express = require('express')
-const app = express()
-const bodyParser = require("body-parser")
-
-app.use(express.static('.'))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.post('/formBuilder', function (req, res) {
-    console.log(req.body)
-    res.send(req.body)
-  })
-
-app.listen(8080, () => console.timeLog('node express 服务器已启动，监听端口：8080'))
-const openDefaultBrowser  = function (url) {
-    var exec = require('child_process').exec;
-    switch (process.platform) {
-        case "darwin":
-            exec('open ' + url);
-            break;
-        case "win32":
-            exec('start ' + url);
-            break;
-        default:
-            exec('xdg-open', [url]);
-    }
-}
-openDefaultBrowser('http://localhost:8080')
+const openDefaultBrowser = function (url) {
+      var exec = require('child_process').exec;
+       switch (process.platform) {
+         case "darwin":
+           exec('open ' + url);
+           break;
+         case "win32":
+           exec('start ' + url);
+           break;
+         default:
+           exec('xdg-open', [url]);
+       }
+    }
+    openDefaultBrowser('http://localhost:8080')
+    
+    var ws=require("nodejs-websocket")
+    let id=0
+    var server=ws.createServer(function(conn){
+        id++
+        conn.name="p"+id
+        broadcast(server,'有新人加入。')
+        conn.on("text", function(str){
+            if(str.slice(0,9)=='nickname|'){
+                conn.name=str.split('|')[1]
+                broadcast(server,conn.name+'上线了。')
+                return
+            }
+            broadcast(server,conn.name+':'+str)
+        })
+        conn.on('connect',function(){
+            conn.name="name"
+        })
+        conn.on("close",function(code,reason){
+            console.log("Connection closed")
+        })
+    }).listen(8081,()=>console.log('socket server listening on:8081'))
+    function broadcast(server, msg){
+        server.connections.forEach(function(conn){
+            conn.sendText(msg)
+        })
+    }
